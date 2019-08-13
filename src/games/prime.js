eb0a59e1c5d71283d0c4a61aa5d5e5cccb20cd20
korol <cx.korol@gmail.com>
2019-08-13 16:10:51 +0300

    feat: add Prime game

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import getRandomNumber from '../utilities';
+import startGame from '..'
+
+const isPrime = number => {
+    if (number <= 1) return false;
+    if (number === 2) return true;
+
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+const gameDiscription = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const makeQuest = () => {
+    const number = getRandomNumber();
+    const question = number;
+    const rightAnswer = isPrime(number) ? 'yes' : 'no';
+
+    return [question, rightAnswer];
+}
+
+export default () => startGame(gameDiscription, makeQuest);
