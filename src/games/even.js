bed78a94e317d1e3fbae81db0a49891d35e5ad12
korol <cx.korol@gmail.com>
2019-08-26 16:52:25 +0300

    style: rename func

--- src/games/even.js
+++ src/games/even.js
@@ -4,11 +4,11 @@ import runGame from '..';
 const isEven = (number) => number % 2 === 0;
 const gameDescription = 'Answer "yes" if number even otherwise answer "no"';
 
-const playIsEvenGame = () => {
+const makeEvenGame = () => {
   const question = getRandomNumber();
   const rightAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, rightAnswer];
 };
 
-export default () => runGame(gameDescription, playIsEvenGame);
+export default () => runGame(gameDescription, makeEvenGame);
